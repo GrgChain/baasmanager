4de63776f05c76d11c9d5e84c5d5c07019ff8da9
罗钜文 <ljwen17@grgbanking.com>
2019-11-21 09:24:39 +0800

    增加cc脚本

---
+++ baas-others/script/rmcccontainer.sh
@@ -0,0 +1,6 @@
+CONTAINER_IDS=$( docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
+if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
+    echo "---- No containers available for deletion ----"
+else
+    docker rm -f $CONTAINER_IDS
+fi
---
+++ baas-others/script/rmccimages.sh
@@ -0,0 +1,6 @@
+DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')
+if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+else
+    docker rmi -f $DOCKER_IMAGE_IDS
+fi
