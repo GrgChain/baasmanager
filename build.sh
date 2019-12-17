cf06ac5109143da5eec00d6fb412d103cf402f30
ljwen17 <ljwen17@grgbanking.com>
2019-12-17 15:37:09 +0800

    ok

--- build.sh
+++ build.sh
@@ -4,17 +4,22 @@ export BASE=$(pwd)
 if [ ! -d "bin" ];then
 mkdir bin
 else
-echo "文件夹已经存在"
+rm -rf bin/*
 fi
-# 编译baas-gateway
+echo "编译baas-gateway"
 cd $BASE/baas-gateway
 go build .
 mv baas-gateway $BASE/bin
-# 编译baas-fabricengine
+echo "编译baas-fabricengine"
 cd $BASE/baas-fabricengine
 go build .
 mv baas-fabricengine $BASE/bin
-# 编译baas-kubeengine
+echo "编译baas-kubeengine"
 cd $BASE/baas-kubeengine
 go build .
 mv baas-kubeengine $BASE/bin
+echo "编译baas-frontend"
+cd $BASE/baas-frontend
+rm -rf node_modules && npm install --registry=https://registry.npm.taobao.org
+npm run build:prod
+mv dist $BASE/bin/baas-frontend
